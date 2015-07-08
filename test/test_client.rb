abonas/openshift_client

commit 0b3d8b2e3dedab64f3df21528807887117e4c21b

Use openshift v1 api as default
Move to openshift v1 api and bump version to 0.0.7

lib/openshift_client.rb
@@ -20,8 +20,8 @@ module OpenshiftClient
       [OpenshiftClient.const_set(et, Class.new(RecursiveOpenStruct)), et]
     end
 
-    def initialize(uri, version = 'v1beta1')
-      handle_uri(uri, '/osapi')
+    def initialize(uri, version = 'v1', path = 'oapi')
+      handle_uri(uri, path)
       @api_version = version
       @headers = {}
       ssl_options

lib/openshift_client/version.rb
@@ -1,4 +1,4 @@
 # Openshift REST-API Client
 module OpenshiftClient
-  VERSION = '0.0.6'
+  VERSION = '0.0.7'
 end

test/test_client.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 # Client general tests
 class TestClient < MiniTest::Test
   def test_api_valid
-    client = OpenshiftClient::Client.new 'https://localhost:8080/osapi'
+    client = OpenshiftClient::Client.new 'https://localhost:8080/oapi'
     assert_equal(true, client.respond_to?('api_valid?'))
   end
 end

test/test_project.rb
@@ -11,14 +11,14 @@ class TestProject < MiniTest::Test
       .to_return(body: open_test_json_file('project_b1.json'),
                  status: 200)
 
-    client = OpenshiftClient::Client.new 'https://localhost:8080/osapi'
-    project = client.get_project 'master'
+    client = OpenshiftClient::Client.new 'https://localhost:8080/oapi'
+    project = client.get_project 'test'
 
     assert_instance_of(OpenshiftClient::Project, project)
-    assert_equal('80359e00-d874-11e4-b110-f8b156af4ae1', project.metadata.uid)
-    assert_equal('master', project.metadata.name)
-    assert_equal('36', project.metadata.resourceVersion)
-    assert_equal('v1beta1', project.apiVersion)
+    assert_equal('2626e632-2491-11e5-9fe4-727174f8ab71', project.metadata.uid)
+    assert_equal('test', project.metadata.name)
+    assert_equal('241', project.metadata.resourceVersion)
+    assert_equal('v1', project.apiVersion)
   end
 
   def test_get_projects
@@ -26,12 +26,12 @@ class TestProject < MiniTest::Test
       .to_return(body: open_test_json_file('project_list_b1.json'),
                  status: 200)
 
-    client = OpenshiftClient::Client.new 'https://localhost:8080/osapi'
+    client = OpenshiftClient::Client.new 'https://localhost:8080/oapi'
     projects = client.get_projects
 
     assert_instance_of(Kubeclient::Common::EntityList, projects)
 
-    assert_equal(2, projects.size)
+    assert_equal(5, projects.size)
     assert_instance_of(OpenshiftClient::Project, projects[1])
   end
 end

test/test_route.rb
@@ -11,12 +11,12 @@ class TestRoute < MiniTest::Test
       .to_return(body: open_test_json_file('route_b1.json'),
                  status: 200)
 
-    client = OpenshiftClient::Client.new 'https://localhost:8080/osapi'
-    route = client.get_route 'master'
+    client = OpenshiftClient::Client.new 'https://localhost:8080/oapi'
+    route = client.get_route 'route-edge'
 
     assert_instance_of(OpenshiftClient::Route, route)
-    assert_equal('99135218-d927-11e4-9471-f8b156af4ae1', route.metadata.uid)
+    assert_equal('6937497c-249d-11e5-9fe4-727174f8ab71', route.metadata.uid)
     assert_equal('route-edge', route.metadata.name)
-    assert_equal('v1beta1', route.apiVersion)
+    assert_equal('v1', route.apiVersion)
   end
 end

commit 00a61e0bc1afc838554c6839293c1f3fb1219a6f

Default path should be absolute
Fix a bug introduced in 0b3d8b2e3dedab64f3df21528807887117e4c21b.
The default path in openshift client ctor must be an absolute path.

lib/openshift_client.rb
@@ -20,7 +20,7 @@ module OpenshiftClient
       [OpenshiftClient.const_set(et, Class.new(RecursiveOpenStruct)), et]
     end
 
-    def initialize(uri, version = 'v1', path = 'oapi')
+    def initialize(uri, version = 'v1', path = '/oapi')
       handle_uri(uri, path)
       @api_version = version
       @headers = {}

lib/openshift_client/version.rb
@@ -1,4 +1,4 @@
 # Openshift REST-API Client
 module OpenshiftClient
-  VERSION = '0.0.7'
+  VERSION = '0.0.8'
 end

test/test_client.rb
@@ -6,4 +6,10 @@ class TestClient < MiniTest::Test
     client = OpenshiftClient::Client.new 'https://localhost:8080/oapi'
     assert_equal(true, client.respond_to?('api_valid?'))
   end
+
+  def test_path_valid
+    uri = URI::HTTPS.build(host: 'localhost', port: 8080)
+    client2 = OpenshiftClient::Client.new uri
+    assert_equal(true, client2.respond_to?('api_valid?'))
+  end
 end